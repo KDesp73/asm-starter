1047e384fc2d4679b5c899098487bbddeb6a8b49
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-09-05 19:36:33 +0300

    added starter

---
+++ Makefile
@@ -0,0 +1,32 @@
+ASM = nasm
+LD = ld
+ASM_FLAGS = -f elf32 -ggdb -F dwarf    # Assembly flags: elf32 format with DWARF debugging info
+LD_FLAGS = -m elf_i386
+
+SRC_DIR = src
+OBJ_DIR = build
+SRC = $(wildcard $(SRC_DIR)/*.asm)
+OBJ = $(patsubst $(SRC_DIR)/%.asm,$(OBJ_DIR)/%.o,$(SRC))
+EXEC = helloworld
+
+all: $(EXEC)
+
+$(OBJ_DIR):
+	mkdir -p $(OBJ_DIR)
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.asm | $(OBJ_DIR)
+	$(ASM) $(ASM_FLAGS) $< -o $@
+
+$(EXEC): $(OBJ)
+	$(LD) $(LD_FLAGS) -o $@ $^
+
+clean:
+	rm -f $(OBJ) $(EXEC)
+
+cleanall: clean
+	rm -rf $(OBJ_DIR)
+
+debug: $(EXEC)
+	gdb $(EXEC)
+
+.PHONY: all clean cleanall debug
---
+++ src/main.asm
@@ -0,0 +1,21 @@
+; helloworld.asm
+
+section .data
+    hello db "Hello World", 13, 10, 0  ; Null-terminated string
+    hello_len equ $ - hello
+
+section .text
+    global _start
+
+_start:
+    ; Write "Hello, World!" to stdout
+    mov eax, 4            ; syscall number for sys_write (0x4)
+    mov ebx, 1            ; file descriptor 1 is stdout
+    mov ecx, hello        ; pointer to the message
+    mov edx, hello_len    ; message length
+    int 0x80              ; interrupt to make the syscall
+
+    ; Exit program
+    mov eax, 1            ; syscall number for sys_exit (0x1)
+    xor ebx, ebx          ; exit code 0
+    int 0x80              ; interrupt to make the syscall
